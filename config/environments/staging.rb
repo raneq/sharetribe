cd8ea32239fd8cbf881d2c49ee94cf28e5ecb810
Pau Perez <saulopefa@gmail.com>
2019-12-17 13:02:56 +0100

    Revert to Rails logging defaults in staging
    This will hopefully will let us debug why there is no log/staging.log.
    That line `config.logger = ::Logger.new(STDOUT)` seems to be the reason.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -41,37 +41,6 @@ Rails.application.configure do
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
-  config.log_level = :info
-  # Basic log config, for calls to Rails.logger.<level> { <message> }
-  config.logger = ::Logger.new(STDOUT)
-  # Formats log entries into: LEVEL MESSAGE
-  # Heroku adds to this timestamp and worker/dyno id, so datetime can be stripped
-  config.logger.formatter = ->(severity, datetime, progname, msg) { "#{severity} #{msg}\n" }
-
-  # Lograge config, overrides default instrumentation for logging ActionController and ActionView logging
-  config.lograge.enabled = true
-  config.lograge.custom_options = ->(event) {
-    params = event.payload[:params].except('controller', 'action')
-
-    { params: params,
-      host: event.payload[:host],
-      community_id: event.payload[:community_id],
-      current_user_id: event.payload[:current_user_id],
-      user_agent: event.payload[:user_agent],
-      referer: event.payload[:referer],
-      forwarded_for: event.payload[:forwarded_for],
-      request_uuid: event.payload[:request_uuid] }
-  }
-
-  config.lograge.formatter = Lograge::Formatters::Json.new
-
-  config.after_initialize do
-    ActiveRecord::Base.logger = Rails.logger.clone
-    ActiveRecord::Base.logger.level = Logger::INFO
-    ActionMailer::Base.logger = Rails.logger.clone
-    ActionMailer::Base.logger.level = Logger::INFO
-  end
-
   # Prefer redis instead of memcached
   config.cache_store =
     if ENV["redis_host"].present?
